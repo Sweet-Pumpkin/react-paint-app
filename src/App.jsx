5ee3e11d904397440e0c820c96af00e9c27d7376
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-07-17 16:48:51 +0900

    feat: Rebuild project

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,7 @@
-import './App.scss';
-import React, { useEffect, useRef } from 'react';
+import React from 'react'
 
-function App() {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    let painting = false;
-
-    const canvas = canvasRef.current;
-    canvas.width = 550;
-    canvas.height = 450;
-
-    const ctx = canvas.getContext("2d");
-    ctx.strokeStyle = "#000";
-    ctx.lineWidth = 2.5;
-
-    canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("mousedown", startDraw);
-    canvas.addEventListener("mouseup", stopDraw);
-    canvas.addEventListener("mouseleave", stopDraw);
-    
-    function draw(event) {
-      const x = event.offsetX;
-      const y = event.offsetY;
-      if (!painting) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-      } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
-      }
-    }
-    function startDraw() {
-      painting = true;
-    }
-    function stopDraw() {
-      painting = false;
-    }
-  }, []);
-
+export default function App() {
   return (
-    <div className="body">
-      <div className="canvasCon">
-        <canvas 
-          ref={canvasRef}
-          className="canvas"
-        >
-        </canvas>
-      </div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
